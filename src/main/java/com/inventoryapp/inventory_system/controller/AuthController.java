00a947fa00b722634d470a8912b3509c5fd29106
Biraj-P <birajpaul18@gmail.com>
2025-11-05 16:00:33 +0530

    Added the DTO for Register and Login

---
+++ src/main/java/com/inventoryapp/inventory_system/controller/AuthController.java
@@ -0,0 +1,5 @@
+package com.inventoryapp.inventory_system.controller;
+
+public class AuthController {
+
+}
--- src/main/java/com/inventoryapp/inventory_system/dto/LoginRequest.java
+++ src/main/java/com/inventoryapp/inventory_system/dto/LoginRequest.java
@@ -4,12 +4,9 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
-public class RegisterRequest {
+public class LoginRequest {
     @NotBlank
     private String username;
     @NotBlank
     private String password;
-
-    //We will default everyone to ROLE_USER for now.
-    // TODO - Add a 'role' field here for admin registration
 }
